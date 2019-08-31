dc71e3e4cc6d4f598bb72871628b411f09a40c1a
Marcus Souchon <marcus_s@live.com.au>
2019-08-31 14:24:53 +1000

    camera movement implemented

---
+++ Assets/CameraMovementScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementScript : MonoBehaviour
+{
+
+    public float movementSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	this.transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * movementSpeed);
+	this.transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * movementSpeed);
+    }
+}
---
+++ Assets/CameraRotationScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotationScript : MonoBehaviour
+{
+
+    public float cameraSensitivity = 1.0f;
+
+    private float pitch = 0.0f;
+    private float yaw = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pitch -= cameraSensitivity * Input.GetAxis("Mouse Y");
+	yaw += cameraSensitivity * Input.GetAxis("Mouse X");
+	this.transform.eulerAngles = new Vector3(pitch, yaw, 0);
+    }
+}
