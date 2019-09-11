74d7c8adb3e8c830ddf8123ee5e01acbeccbe49e
k-start <startkie@gmail.com>
2019-09-11 18:49:20 +1000

    Phongs on terrain

---
+++ Assets/PointLight.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointLight : MonoBehaviour {
+
+    public Color color;
+    public Material material;
+
+    void Update() {
+        material.SetColor("_PointLightColor", color);
+        material.SetVector("_PointLightPosition", transform.position);
+    }
+}
---
+++ Assets/RotateLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLight : MonoBehaviour
+{
+
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.back * (speed * Time.deltaTime));
+    }
+}
