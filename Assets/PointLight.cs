a05b7e8e22d999049f489a89f003fd2941c7e2a5
k-start <startkie@gmail.com>
2019-09-13 17:08:53 +1000

    fade to black

--- Assets/PointLight.cs
+++ Assets/PointLight.cs
@@ -10,14 +10,18 @@ public class PointLight : MonoBehaviour {
     //public Material material;
 
     void Update() {
-	foreach (Material material in materialList) {
-        if(transform.position.y < 200.0f) {
-            material.SetColor("_PointLightColor", Color.Lerp(colorLow, color, transform.position.y / 200.0f));
-        }else {
-            material.SetColor("_PointLightColor", color);
-        }
+	    foreach (Material material in materialList) {
+            // Change colour of light depending on height
+            if (transform.position.y < 200.0f && transform.position.y >= 0.0f) {
+                material.SetColor("_PointLightColor", Color.Lerp(colorLow, color, transform.position.y / 200.0f));
+            } else if (transform.position.y < 0.0f) {
+                // quickly fade to black
+                material.SetColor("_PointLightColor", Color.Lerp(colorLow, Color.black, -(transform.position.y / 200.0f)));
+            } else {
+                material.SetColor("_PointLightColor", color);
+            }
         
-        material.SetVector("_PointLightPosition", transform.position);
-	}
+            material.SetVector("_PointLightPosition", transform.position);
+	    }
     }
 }
