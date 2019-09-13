60085483792f052ad7c7fe31534c1e52b4a9070e
Marcus Souchon <marcus_s@live.com.au>
2019-09-13 16:43:49 +1000

    implemented the water shader, just the lighting, textures, and tiling

--- Assets/PointLight.cs
+++ Assets/PointLight.cs
@@ -6,15 +6,11 @@ public class PointLight : MonoBehaviour {
     public Color color;
     public Color colorLow;
     
-    public float Kd = 1;
-    public float Ks = 1;
-    public float specN = 1;
-    public float Ka = 1;
-    public float fAtt = 1;
-
-    public Material material;
+    public Material[] materialList;
+    //public Material material;
 
     void Update() {
+	foreach (Material material in materialList) {
         if(transform.position.y < 200.0f) {
             material.SetColor("_PointLightColor", Color.Lerp(colorLow, color, transform.position.y / 200.0f));
         }else {
@@ -22,10 +18,6 @@ public class PointLight : MonoBehaviour {
         }
         
         material.SetVector("_PointLightPosition", transform.position);
-        material.SetFloat("_Ks", Ks);
-        material.SetFloat("_specN", specN);
-        material.SetFloat("_Kd", Kd);
-        material.SetFloat("_Ka", Ka);
-        material.SetFloat("_fAtt", fAtt);
+	}
     }
 }
