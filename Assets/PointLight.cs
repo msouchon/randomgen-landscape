367bd3e53df4283bb80bfcb2f491166c30ab9875
k-start <startkie@gmail.com>
2019-09-13 12:35:08 +1000

    Colour interpolation for sun

--- Assets/PointLight.cs
+++ Assets/PointLight.cs
@@ -4,10 +4,16 @@ using System.Collections;
 public class PointLight : MonoBehaviour {
 
     public Color color;
+    public Color colorLow;
     public Material material;
 
     void Update() {
-        material.SetColor("_PointLightColor", color);
+        if(transform.position.y < 200.0f) {
+            material.SetColor("_PointLightColor", Color.Lerp(colorLow, color, transform.position.y / 200.0f));
+        }else {
+            material.SetColor("_PointLightColor", color);
+        }
+        
         material.SetVector("_PointLightPosition", transform.position);
     }
 }
