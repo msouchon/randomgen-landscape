a87d63de7274c48eb81529d6212ef4f9d8d20ab5
Marcus Souchon <marcus_s@live.com.au>
2019-09-14 16:10:29 +1000

    file structure and commenting

--- Assets/CameraMovementScript.cs
+++ Assets/CameraMovementScript.cs
@@ -8,25 +8,21 @@ public class CameraMovementScript : MonoBehaviour
     public float movementSpeed = 100.0f;
     public float sprintFactor = 2.0f;
 
-    public Rigidbody rigidbody;
+    public Rigidbody rb;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-	    float speed = movementSpeed;
-	    if (Input.GetButton("Fire1")) speed *= sprintFactor;
+	// Allow increased movement speed
+	float speed = movementSpeed;
+	if (Input.GetButton("Fire1")) speed *= sprintFactor;
 
         Vector3 vertical, horizonal, jump;
         vertical = transform.forward * Input.GetAxis("Vertical") * speed;
         horizonal = transform.right * Input.GetAxis("Horizontal") * speed;
+
+	// Add extra movement for going directly up and down in relation to the world
         jump = Vector3.up * Input.GetAxis("Jump") * speed;
 
-        rigidbody.velocity = vertical + horizonal + jump;
+        rb.velocity = vertical + horizonal + jump;
     }
 }
